bfee2f90c9c34f053d124e84ba9c5182c782681e
Glenn Halls <gjhalls@gmail.com>
2022-08-22 14:10:45 +1000

    Add: buttons with listeners

--- app/src/main/java/com/example/coins_n_dice/MainActivity.kt
+++ app/src/main/java/com/example/coins_n_dice/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.coins_n_dice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Set on click listeners for buttons
+        val blackDieButton: Button = findViewById(R.id.button_black_die)
+        blackDieButton.setOnClickListener { catLog(blackDieButton) }
+
+        val redDieButton: Button = findViewById(R.id.button_red_die)
+        redDieButton.setOnClickListener { catLog(redDieButton) }
+
+        val goldCoinButton: Button = findViewById(R.id.button_gold_coin)
+        goldCoinButton.setOnClickListener { catLog(goldCoinButton) }
+
+        val silverCoinButton: Button = findViewById(R.id.button_silver_coin)
+        silverCoinButton.setOnClickListener { catLog(silverCoinButton) }
+
+    }
+
+    private fun catLog(value: Button) {
+        Log.d("init", "clicked on ${value.resources.getResourceName(value.id)}")
     }
 }
